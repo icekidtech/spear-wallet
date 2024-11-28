be55bb3ba1c33e26dd941ce0ed7f3c3abb819683
icekidtech <princeklvn7568@gmail.com>
2024-11-29 00:49:58 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.database import database
+from app.routes.routes import router  # Import the router
 
 app = FastAPI()
+
+app.include_router(router, prefix="/api/v1")  # Include the router with a prefix
 
 @app.on_event("startup")
 async def startup():
@@ -14,3 +17,4 @@ async def shutdown():
 @app.get("/")
 async def root():
     return {"message": "Welcome to Spear Wallet Backend!"}
+
