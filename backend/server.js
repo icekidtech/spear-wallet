d353fe83299ed3beac7721246f021f1ab5b2e3a4
icekidtech <princeklvn7568@gmail.com>
2024-12-09 01:21:29 +0100

    added db auth

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import passport from "passport";
+import bodyParser from 'body-parser';
 import authRoutes from "./src/routes/authRoutes.js";
 import routes from './src/routes/index.js'; // Renamed router to routes
 import { connectDB } from './src/config/db.js';
@@ -13,17 +14,23 @@ const PORT = process.env.PORT || 3000;
 // Routes
 app.use('/api/auth', authRoutes);
 
-// Middleware
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(passport.initialize());
+app.use(express.urlencoded({ extended: true }));
 
 // API Routes
 app.get("/", (req, res) => {
-    res.send("Welcome to Spear Wallet API!");
+    res.send("Welcome to Spear Wallet");
   });
   
 app.use("/api", routes); // Now using the correctly imported routes
 app.use("/api/auth", authRoutes);
+
+// Connect to the database
+db.authenticate()
+  .then(() => console.log('Database connected successfully.'))
+  .catch((err) => console.error('Database connection error:', err));
 
 // Start the server
 app.listen(PORT, async () => {
